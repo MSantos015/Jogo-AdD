eac292b88d037016f751a122a1023ec623991c43
Marcelo Santos <lolnaticos.com@gmail.com>
2018-08-20 17:04:21 -0300

    Monster Patrol - Parte 2

--- Assets/Scripts/MonsterPatrolCtrl.cs
+++ Assets/Scripts/MonsterPatrolCtrl.cs
@@ -8,20 +8,29 @@ public class MonsterPatrolCtrl : MonoBehaviour {
 	public float speed=2f;
 	public float waitTime=2f;
 	Vector3 nextPos;
+	Animator anim;
+	SpriteRenderer sr;
 	void Start () {
+		anim= GetComponent<Animator>();
+		sr=GetComponent<SpriteRenderer>();
 		nextPos=Pos1.position;
 		StartCoroutine(Move());
 	}
 	IEnumerator Move(){
-		while(true){
+		while(true){   
 			if (transform.position ==Pos1.position){
 				nextPos=Pos2.position;
+				anim.SetInteger("State",1);
 				yield return new WaitForSeconds(waitTime);
+				anim.SetInteger("State",0);
+				sr.flipX= !sr.flipX;
 			}
 			if (transform.position ==Pos2.position){
 				nextPos=Pos1.position;
+				anim.SetInteger("State",1);
 				yield return new WaitForSeconds(waitTime);
-
+				anim.SetInteger("State",0);
+				sr.flipX= !sr.flipX;
 			}
 			transform.position=Vector3.MoveTowards(transform.position, nextPos, speed*Time.deltaTime);
 			yield return null;
