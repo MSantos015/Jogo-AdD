8a2fa45cfbc366e29bfdfb9f2e03badc14c6ba70
Marcelo Santos <lolnaticos.com@gmail.com>
2018-08-24 08:57:08 -0300

    Machucando o player

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -11,6 +11,7 @@ public class GM : MonoBehaviour {
 	public GameObject playerPrefab;
 	PlayerCtrl player;
 	public float timeToRespawn =2f;
+	public float timeToKill= 1f;
 
 	public float maxTime=120f;
 	bool timerOn=true;
@@ -113,6 +114,31 @@ public class GM : MonoBehaviour {
 		ui.levelComplete.txtCoinCount.text="Coins: "+data.coinCount;
 		ui.levelComplete.txtTimer.text="Timer: "+timeLeft.ToString("F0");
 		ui.levelComplete.levelCompletePanel.SetActive(true);
+	}
+	public void HurtPlayer(){
+		if(player!=null){
+			DisableAndPushPlayer();
+			Destroy (player.gameObject, timeToKill);
+			DecrementLives();
+			if(data.lifeCount>0){
+				Invoke("RespawnPlayer",timeToKill+ timeToRespawn);
+			}
+			else{
+				GameOver();
+			}
+		}
+	}
+	void DisableAndPushPlayer(){
+		player.transform.GetComponent<PlayerCtrl>().enabled=false;
+		foreach(Collider2D c2d in player.transform.GetComponents<Collider2D>()){
+			c2d.enabled=false;
+		}
+		foreach(Transform child in player.transform){
+			child.gameObject.SetActive(false);
+		}
+		Rigidbody2D rb=player.GetComponent<Rigidbody2D>();
+		rb.velocity=Vector2.zero;
+		rb.AddForce(new Vector2(-150.0f,400f));
 	}
 }
 	
