3227b23610bc625e92a7cf2d614a52c4715b887e
Nycolas <nycolasmoura41@gmail.com>
2018-11-21 16:20:28 -0200

    Enemy Spawn Points

---
+++ Assets/Scripts/EnemySpawnPointCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPointCtrl : MonoBehaviour {
+
+
+
+	public GameObject prefab;
+
+
+	public float objSpeed = 2f;
+
+	public float waitTime = 5f;
+	public Vector2[] directions;
+
+	void Start () {
+		StartCoroutine(Spawn());
+		
+	}
+
+
+	IEnumerator Spawn() {
+		while (true){
+			for (int i=0; i<directions.Length; i++) {
+				GameObject obj = Instantiate(prefab, transform.position, Quaternion.identity);
+				obj.GetComponent<SkelletonCtrl>().direction = directions [i];
+				obj.GetComponent<SkelletonCtrl>().speed = objSpeed;
+			}
+			yield return new WaitForSeconds(waitTime);
+		}
+	}
+	
+}
---
+++ Assets/Scripts/SkelletonCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkelletonCtrl : MonoBehaviour {
+
+
+	public float speed = 2f;
+
+	public Vector2 direction = new Vector2(-1f, 0f);
+
+	// Use this for initialization
+
+
+	Rigidbody2D rb;
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move(); 
+		
+	}
+
+	void Move() {
+		rb.velocity = speed * direction.normalized;
+
+	}
+}
